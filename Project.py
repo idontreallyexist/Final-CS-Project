369e6939543d5d9c4a6c301131a785fc640592ed
idontreallyexist <kevinalexyu09@gmail.com>
2025-05-13 22:06:22 +0000

    Started page switching functionality

--- Project.py
+++ Project.py
@@ -6,6 +6,7 @@ import datetime
 pygame.init()
 screen=pygame.display.set_mode((640,480))
 pygame.display.set_caption("Physical Files")
+page=0
 
 while True:
     pygame.display.update()
@@ -13,3 +14,8 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                page+=1
+            if event.key == K_LEFT:
+                page-=1

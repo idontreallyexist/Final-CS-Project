a3d5f4db155ba5e272e8af824cd3dd6c6f3fef3e
idontreallyexist <kevinalexyu09@gmail.com>
2025-05-15 14:20:21 -0700

    Added more complex scaling

--- Project.py
+++ Project.py
@@ -4,34 +4,45 @@ import time
 import datetime
 
 class object:
-    def __init__(self,x,y,scalex,scaley):
+    def __init__(self,x,y,scaleX,scaleY,sFactors):
         self.x=x
         self.y=y
-        self.scalex=scalex
-        self.scaley=scaley
+        self.scaleX=scaleX
+        self.scaleY=scaleY
+        self.sFactors=sFactors
+
+    def scaleUp(self):
+        global page
+        self.scaleX*=self.sFactors[page]
+        self.scaleY*=self.sFactors[page]
+    
+    def scaleDown(self):
+        global page
+        self.scaleX/=self.sFactors[page]
+        self.scaleY/=self.sFactors[page]
 
 pygame.init()
 screen=pygame.display.set_mode((640,480))
 pygame.display.set_caption("Physical Files")
-page=0
-test=object(100,100,1,1)
+page=1
+test=object(100,100,1,1,[1.0,1.1,2,1.5])
 
 while True:
-    pygame.draw.rect(screen,"red",(test.x,test.y,100*test.scalex,100*test.scaley),100)
+    pygame.draw.rect(screen,"red",(test.x,test.y,100*test.scaleX,100*test.scaleY),100*max(test.scaleX,test.scaleY))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
-                page+=1
-                test.x+=100
-                test.scalex*=1.1
-                test.scaley*=1.1
+                if page<3:
+                    page+=1
+                    test.x+=100
+                    test.scaleUp()
             if event.key == K_LEFT:
-                page-=1
-                test.x-=100
-                test.scalex/=1.1
-                test.scaley/=1.1
+                if page>0:
+                    test.scaleDown()
+                    page-=1
+                    test.x-=100
     pygame.display.update()
     screen.fill("black")
